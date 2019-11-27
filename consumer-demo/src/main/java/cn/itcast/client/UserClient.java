08e99e2b7a2c458e61734287fa084048876abd26
linfayang <linfayang@live.com>
2019-11-27 09:46:46 +0800

    简单过滤器实现

---
+++ consumer-demo/src/main/java/cn/itcast/client/UserClient.java
@@ -0,0 +1,13 @@
+package cn.itcast.client;
+
+import cn.itcast.pojo.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "user-service",fallback = UserClientImpl.class )
+public interface UserClient {
+
+    @GetMapping("/user/{id}")
+    String queryById(@PathVariable("id") Integer id);
+}
